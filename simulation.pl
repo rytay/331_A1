c6928e4154298ce31c6121e7abd87bba8a8c5e87
Ryan Taylor <rytay123@gmail.com>
2019-02-04 18:30:17 -0500

    Create simulation.pl
    First draft

---
+++ simulation.pl
@@ -0,0 +1,63 @@
+%% =============================================================================
+%%
+%%  CONCORDIA UNIVERSITY
+%%  Department of Computer Science and Software Engineering
+%%  SOEN 331-W:  Assingment 1
+%%  Winter term, 2019
+%%  Date submitted: February 11, 2019
+%%
+%%  Authors:
+%%
+%%  Rea Sharma, 40029086, reasharma091@hotmail.com
+%%  name2, id2, email2
+%%  ...
+%%
+%% =============================================================================
+
+%% =============================================================================
+%%  Facts
+%%==============================================================================
+
+%%  Top Level States
+    state(idle).
+    state(exit).
+    
+%%  States under active
+    state('warming up').
+    state(configuring).
+   
+
+%%  Initial States
+    initial_state(idle, null)
+
+%% =============================================================================
+%% Transitions(source, destination, event, guard)
+%%==============================================================================
+   
+   
+   transition(idle, idle, null, null, null).
+   transition(idle, configuring, configure, true, 'single beep/turn light on').
+   
+   transition(configuring, idle, complete, true, 'double beep/turn light off').
+   transition(configuring, idle, cancel, true, 'prolonged beep/turn light off').
+   transition(configuring, idle, 'after (1 min)', 'inactive', 'turn light off').
+   
+   transition(idle, idle, 'after (2 min)', 'furnace temperature >= desired temperature', null).
+   transition(idle, 'warming up', 'after (2 min)', 'furnace temperature < desired temperature', 'turn on furnace').
+   
+   transition('warming up', configuring, interupt, true , 'turn off furnace').
+   transition('warming up', idle, 'after (3 min)', 'furnace temperature > desired temperature', 'turn on click/turn on fan').
+   transition('warming up', 'warming up', 'after (3 min)', 'furnace temperature < desired temperature', null).
+   transition(idle, exit, 'shut off' , true, 'turn off fan/turn off furnace').
+
+%% =============================================================================
+%%  Rules
+%%==============================================================================
+
+    ancestor(State):-
+    get_all_transitions(Event, Guard):- .....,Event\=='null', Guard\=='null'
+    get_inherited_transitions():-
+
+
+%% =============================================================================
+%% eof.
